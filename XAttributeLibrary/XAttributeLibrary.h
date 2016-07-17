8dd3b634238e7eb819689c248e222ec9e5dad48e
Chris Armour <chris.armour@gmail.com>
2016-07-17 12:43:50 +0100

    Initial Commit

---
+++ XAttributeLibrary/XAttributeLibrary.h
@@ -0,0 +1,13 @@
+//
+//  XAttributeLibrary.h
+//  XAttributeLibrary
+//
+//  Created by Chris Armour on 17/07/2016.
+//  Copyright © 2016 McLaren Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XAttributeLibrary : NSObject
+
+@end
---
+++ XAttributeLibrary/XAttributeLibrary.m
@@ -0,0 +1,13 @@
+//
+//  XAttributeLibrary.m
+//  XAttributeLibrary
+//
+//  Created by Chris Armour on 17/07/2016.
+//  Copyright © 2016 McLaren Software. All rights reserved.
+//
+
+#import "XAttributeLibrary.h"
+
+@implementation XAttributeLibrary
+
+@end
